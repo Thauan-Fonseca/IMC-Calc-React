20f198673b861cb47c2eeaa41994efd7afbb8bd2
Thauan-Fonseca <126363662+Thauan-Fonseca@users.noreply.github.com>
2023-06-29 16:47:25 -0300

    Project Finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,57 @@
 import './App.css';
-import ImcCalc from './components/imcCalc.jsx';
+import { useState } from 'react';
+import { data } from './data/data.js';
+import ImcTable from './components/ImcTable';
+import ImcCalc from './components/imcCalc';
 
 function App() {
 
+  const calcImc = (e, height, weight) => {
+    e.preventDefault();
+
+    console.log(height, weight)
+
+    const weightFloat = +weight.replace(',', '.');
+    const heightFloat = +height.replace(',', '.');
+
+    const imcResult = (weightFloat / (heightFloat * heightFloat)).toFixed(1)
+
+    if (!weight || !height) return;
+
+    setImc(imcResult);
+
+    data.forEach((item) => {
+      if (imcResult >= item.min && imcResult <= item.max) {
+        setInfo(item.info);
+        setInfoClass(item.infoClass)
+      }
+    });
+
+    if (!info) return;
+
+  }
+
+  const resetCalc = (e) => {
+    e.preventDefault();
+
+    setImc('');
+    setInfo('');
+    setInfoClass('');
+  }
+
+  const [imc, setImc] = useState('');
+  const [info, setInfo] = useState('');
+  const [infoClass, setInfoClass] = useState('');
+
   return (
     <div className='container'>
-      <ImcCalc />
+      {!imc ? < ImcCalc calcImc={calcImc} /> : <ImcTable 
+        data={data} 
+        imc={imc} 
+        info={info} 
+        infoClass={infoClass} 
+        resetCalc={resetCalc}
+      />}
     </div>
   )
 }
--- src/components/imcCalc.jsx
+++ src/components/imcCalc.jsx
@@ -1,6 +1,34 @@
-import './imcCalc.css'
+import { useState } from 'react';
+import './imcCalc.css';
+import Button from './Button';
 
-const ImcCalc = () => {
+const ImcCalc = ({calcImc}) => {
+
+    const [height, setHeight] = useState('');
+    const [weight, setWeight] = useState('');
+
+    const clearForm = (e) => {
+        e.preventDefault();
+
+        setHeight('');
+        setWeight('');
+    }
+
+    const validDigits = (text) =>{
+       return text.replace(/[^0-9,]/g, "");
+    }
+
+    const handleHeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value)
+        setHeight(updatedValue);
+    };
+
+    const handleWeightChange = (e) => {
+        const updatedValue = validDigits(e.target.value);
+
+        setWeight(updatedValue);
+    };
+
   return (
     <div className='calc-container'>
         <h2>Calculadora de IMC</h2>
@@ -8,16 +36,22 @@ const ImcCalc = () => {
             <div className='form-inputs'>
                 <div className='form-control'>
                     <label htmlFor="height">Altura:</label>
-                    <input type="text" name="height" placeholder="Exemplo 1,76" />
+                    <input type="text" name="height" placeholder="Exemplo 1,76" 
+                    onChange={(e) => handleHeightChange(e)}
+                    value={height}
+                    />
+
+                    <label htmlFor="weight">Peso:</label>
+                    <input type="text" name="weight" placeholder="Exemplo 90" 
+                    onChange={(e) => handleWeightChange(e)}
+                    value={weight}
+                    />
                 </div>
-                <div>
-                <label htmlFor="weight">Peso:</label>
-                    <input type="text" name="weight" placeholder="Exemplo 90" />
-                </div>
+                
             </div>
             <div className="action-control">
-                <button>Calcular</button>
-                <button>Limpar</button>
+                <Button id="calc-btn" text="Calcular" action={(e) => calcImc(e, height, weight)}/>
+                <Button id="clear-btn" text="Limpar" action={clearForm}/>
             </div>
         </form>
     </div>
